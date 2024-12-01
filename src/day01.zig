ea1343cf22a4a2fc546fe7d2eb35568cac458f76
Shahruk Hossain <shahruk10@gmail.com>
2024-12-02 04:21:03 +0600

    refactor: improve day01
      - Use a struct to store IDs and counts in sorted manner to make it
        simpler and faster to compute the pair distances and look up counts.

--- src/day01.zig
+++ src/day01.zig
@@ -2,46 +2,145 @@ const std = @import("std");
 
 const data = @embedFile("data/day01.txt");
 
-pub fn main() !void {
-    const N = comptime 1000;
-    var left_list: [N]i32 = undefined;
-    var right_list: [N]i32 = undefined;
+const N = 1000;
 
-    var lines = std.mem.tokenizeAny(u8, data, "\n");
-    var n: usize = 0;
+const Errors = error{
+    CapacityExceeded,
+};
 
-    while (lines.next()) |line| {
-        var numbers = std.mem.tokenizeAny(u8, line, " ");
+const LocationCount = struct {
+    id: i32,
+    count: i32,
+};
 
-        left_list[n] = try std.fmt.parseInt(i32, numbers.next().?, 10);
-        right_list[n] = try std.fmt.parseInt(i32, numbers.next().?, 10);
-        n += 1;
+/// LocationList stores a list of location IDs, always maintaining the list in
+/// an ascending order of IDs.
+const LocationList = struct {
+    ids: [N]i32 = undefined,
+    len_ids: usize = 0,
+
+    counts: [N]LocationCount = undefined,
+    len_counts: usize = 0,
+
+    /// Stores the given ID within the list of locations.
+    pub fn store(self: *LocationList, id: i32) !void {
+        if (self.len_ids >= N) {
+            return Errors.CapacityExceeded;
+        }
+
+        self.storeID(id);
+        self.updateCount(id);
     }
 
-    std.sort.block(i32, left_list[0..n], {}, std.sort.asc(i32));
-    std.sort.block(i32, right_list[0..n], {}, std.sort.asc(i32));
+    /// Returns the number of times the given location ID appears within the list.
+    pub fn getCount(self: *LocationList, id: i32) i32 {
+        var a: usize = 0;
+        var b: usize = self.len_counts;
 
-    var ans_part1: u32 = 0;
-    var ans_part2: u64 = 0;
+        // Using binary search as count list is sorted by IDs.
+        while (a < b) {
+            const i = (a + b) / 2;
 
-    for (0..n) |i| {
-        ans_part1 += @abs(left_list[i] - right_list[i]);
-
-        var freq: i32 = 0;
-
-        for (0..n) |j| {
-            if (right_list[j] > left_list[i]) {
-                break;
-            }
-
-            if (right_list[j] == left_list[i]) {
-                freq += 1;
+            if (self.counts[i].id < id) {
+                a = i + 1;
+            } else if (self.counts[i].id > id) {
+                b = i;
+            } else {
+                return self.counts[i].count;
             }
         }
 
-        ans_part2 += @intCast(freq * left_list[i]);
+        return 0;
     }
 
-    std.debug.print("part 1: sum of distances = {d}\n", .{ans_part1});
-    std.debug.print("part 2: similarity score = {d}\n", .{ans_part2});
+    /// Computes the distance between location IDs of this list and the other provided list.
+    pub fn computeDistance(self: *LocationList, other: *LocationList) u32 {
+        var n: usize = self.len_ids;
+        if (n > other.len_ids) {
+            n = other.len_ids;
+        }
+
+        var sum_distance: u32 = 0;
+
+        for (0..n) |i| {
+            sum_distance += @abs(self.ids[i] - other.ids[i]);
+        }
+
+        return sum_distance;
+    }
+
+    /// Computes the similarity score between location IDs of this list and the other provided list.
+    pub fn computeSimilarity(self: *LocationList, other: *LocationList) i32 {
+        var sum_similarity: i32 = 0;
+
+        for (0..self.len_ids) |i| {
+            sum_similarity += (self.ids[i] * other.getCount(self.ids[i]));
+        }
+
+        return sum_similarity;
+    }
+
+    /// Stores the given ID in the list maintaining sorted order.
+    fn storeID(self: *LocationList, id: i32) void {
+        for (0..self.len_ids) |i| {
+            if (id <= self.ids[i]) {
+                shiftRightFrom(i, i32, self.ids[0 .. self.len_ids + 1]);
+                self.ids[i] = id;
+                self.len_ids += 1;
+
+                return;
+            }
+        }
+
+        self.ids[self.len_ids] = id;
+        self.len_ids += 1;
+    }
+
+    /// Updates teh count for the given ID.
+    fn updateCount(self: *LocationList, id: i32) void {
+        for (0..self.len_counts) |i| {
+            if (self.counts[i].id == id) {
+                self.counts[i].count += 1;
+                return;
+            }
+
+            if (id < self.counts[i].id) {
+                shiftRightFrom(i, LocationCount, self.counts[0 .. self.len_counts + 1]);
+                self.counts[i] = LocationCount{ .id = id, .count = 1 };
+                self.len_counts += 1;
+
+                return;
+            }
+        }
+
+        self.counts[self.len_counts] = LocationCount{ .id = id, .count = 1 };
+        self.len_counts += 1;
+    }
+
+    /// Shifts the given array one element to the right, starting at i'th index.
+    fn shiftRightFrom(i: usize, comptime T: type, arr: []T) void {
+        var j: usize = arr.len - 1;
+        while (j >= i and j > 0) : (j -= 1) {
+            arr[j] = arr[j - 1];
+        }
+    }
+};
+
+pub fn main() !void {
+    var left_list = LocationList{};
+    var right_list = LocationList{};
+
+    var lines = std.mem.tokenizeAny(u8, data, "\n");
+
+    while (lines.next()) |line| {
+        var numbers = std.mem.tokenizeAny(u8, line, " ");
+        try left_list.store(try std.fmt.parseInt(i32, numbers.next().?, 10));
+        try right_list.store(try std.fmt.parseInt(i32, numbers.next().?, 10));
+    }
+
+    const ans_part1 = left_list.computeDistance(&right_list);
+    const ans_part2 = left_list.computeSimilarity(&right_list);
+
+    std.debug.print("day01\tpart 1\t{d}\n", .{ans_part1});
+    std.debug.print("day01\tpart 2\t{d}\n", .{ans_part2});
 }
