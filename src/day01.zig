0af28b0d56884d8da669153e17ccd57ac160c552
Shahruk Hossain <shahruk10@gmail.com>
2024-12-01 19:21:38 +0600

    feat: solve day01

--- src/day01.zig
+++ src/day01.zig
@@ -1,46 +1,47 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
 
 const data = @embedFile("data/day01.txt");
 
 pub fn main() !void {
-    
+    const N = comptime 1000;
+    var left_list: [N]i32 = undefined;
+    var right_list: [N]i32 = undefined;
+
+    var lines = std.mem.tokenizeAny(u8, data, "\n");
+    var n: usize = 0;
+
+    while (lines.next()) |line| {
+        var numbers = std.mem.tokenizeAny(u8, line, " ");
+
+        left_list[n] = try std.fmt.parseInt(i32, numbers.next().?, 10);
+        right_list[n] = try std.fmt.parseInt(i32, numbers.next().?, 10);
+        n += 1;
+    }
+
+    std.sort.block(i32, left_list[0..n], {}, std.sort.asc(i32));
+    std.sort.block(i32, right_list[0..n], {}, std.sort.asc(i32));
+
+    var ans_part1: u32 = 0;
+    var ans_part2: u64 = 0;
+
+    for (0..n) |i| {
+        ans_part1 += @abs(left_list[i] - right_list[i]);
+
+        var freq: i32 = 0;
+
+        for (0..n) |j| {
+            if (right_list[j] > left_list[i]) {
+                break;
+            }
+
+            if (right_list[j] == left_list[i]) {
+                freq += 1;
+            }
+        }
+
+        ans_part2 += @intCast(freq * left_list[i]);
+    }
+
+    std.debug.print("part 1: sum of distances = {d}\n", .{ans_part1});
+    std.debug.print("part 2: similarity score = {d}\n", .{ans_part2});
 }
-
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
